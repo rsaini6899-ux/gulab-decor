1e128ac2b3edd3232ac932273682861d9378ab00
ravi <rsaini6899@gmail.com>
2026-02-21 10:50:07 +0530

    server.js change url

--- server.js
+++ server.js
@@ -36,7 +36,9 @@ app.use(cors({
   origin: [
     process.env.FRONTEND_URL,
     'http://localhost:5173',
-    'http://localhost:5174'
+    'http://localhost:5174',
+    'https://gulabi-decore.netlify.app',
+    'https://gulabi-decore-admin.netlify.app'
   ].filter(Boolean),
   credentials: true
 }));
